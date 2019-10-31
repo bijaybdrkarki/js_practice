7a863f267c6ed6cb52235c2181072c3b57d74963
Bijay Karki <n01271571@nj-00118-019.humber.org>
2019-10-31 11:48:04 -0400

    solved some sample qns

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+let jobTitle = `programmer`;
+let geographiclocation = `Brampton`;
+let annualSalary = 100000;
+let compayName = `Humber`;
+console.log(`you will be a `+ jobTitle + ` in ` + geographiclocation + ` making ` + annualSalary + ` for `+ compayName);
+
+
+let curYear = 2019;
+let dob = 1990;
+console.log(`you are ${curYear-dob} years old`); 
+
+let curAge = 29;
+let maxAge = 80;
+let perDay = 3;
+console.log(`you will need ${perDay*365*(maxAge-curAge)} to last until the age of ${maxAge}`);
+
+let radius = 7;
+console.log(`the circumferernce is ${2*22/7*radius}`);
+console.log(`the area is ${22/7*radius*radius}`);
+
+let temp = 25;
+console.log(`${temp} C is equivalent to ${(temp*9/5)+32} F`);
